c7a29d25443fccdbe3c90802a517eeccbfd0868c
Ethan <ethan-degross@uiowa.edu>
2019-07-26 13:46:51 -0500

    Added test to check for the inventory list's keys

--- test/index.test.js
+++ test/index.test.js
@@ -7,4 +7,9 @@ describe('Store Inventory', () => {
     it('exists', () => {
         expect(inventoryList).to.exist
     })
+    it('has the correct keys', () => {
+        expect(inventoryList[0]).to.be.an('object').includes.keys("name", "price", "markdown", "special")
+        expect(inventoryList[0].markdown).include.keys("decrease", "limit")
+        expect(inventoryList[0].special).include.keys("type", "limit")
+    })
 })
