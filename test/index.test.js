41939dec2c717cf3d583dabcfd484055d13ae0c6
Ethan <ethan-degross@uiowa.edu>
2019-07-26 12:19:28 -0500

    Created a store inventory list

--- test/index.test.js
+++ test/index.test.js
@@ -1,3 +1,10 @@
 import chai from 'chai'
 import { expect } from 'chai'
 import * as F from '../src/index'
+import { inventoryList } from './mock-data'
+
+describe('Store Inventory', ()=> {
+    it('has invidual item information', () => {
+        expect(inventoryList).to.exist
+    })
+})
---
+++ test/mock-data.js
@@ -0,0 +1,12 @@
+export const inventoryList = [{
+    "name":"bread",
+    "price":4.32,
+    "markdown":{
+        "decrease":.50,
+        "limit":5
+    },
+    "special":{
+        "type":"buy 2, get 1 free",
+        "limit":6
+    }
+}]
