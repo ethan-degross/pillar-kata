aed85801a00afef4472655cdb7ef46d792fd866e
Ethan <ethan-degross@uiowa.edu>
2019-07-31 09:52:09 -0500

    Refactored code to use array.find()
    Previously was using the specific index in the test

--- test/index.test.js
+++ test/index.test.js
@@ -14,27 +14,34 @@ describe('Store Inventory', () => {
     })
     it('added an item', () => {
         expect(F.addItemToInventory(inventoryList, "chips", 1.99, null, null, null, null)).to.be.an('array')
-        expect(inventoryList[0].itemName).to.include("chips")
+        expect(inventoryList.find(item => item.itemName === "chips").itemName).to.include("chips")
     })
 })
 
 describe('Functions that edit the store inventory', () => {
     it('Item edited', () => {
         expect(F.editItem(inventoryList, "bread", "itemPrice", 10.99)).to.be.an('array')
-        expect(inventoryList[1].itemPrice).to.equal(10.99)
+        expect(inventoryList.find(item => item.itemName === "bread").itemPrice).to.equal(10.99)
 
         F.editItem(inventoryList, "bread", "itemName", "whole wheat bread")
-        expect(inventoryList[1].itemName).to.equal("whole wheat bread")
+        expect(inventoryList.find(item => item.itemName === "whole wheat bread")).to.not.be.undefined
 
         F.editItem(inventoryList, "chips", "itemPrice", 2.99)
-        expect(inventoryList[0].itemPrice).to.equal(2.99)
+        expect(inventoryList.find(item => item.itemName === "chips").itemPrice).to.equal(2.99)
     })
     it('Edited the nested keys', () => {
         F.editItem(inventoryList, "chips", "itemMarkdown.decrease", 1)
-        expect(inventoryList[0].itemMarkdown.decrease).to.equal(1)
+        expect(inventoryList.find(item => item.itemName === "chips").itemMarkdown.decrease).to.equal(1)
 
         F.editItem(inventoryList, "whole wheat bread", "itemMarkdown.limit", 5)
-        expect(inventoryList[1].itemMarkdown.limit).to.equal(5)
+        expect(inventoryList.find(item => item.itemName === "whole wheat bread").itemMarkdown.limit).to.equal(5)
     })
 })
 
+describe('Helper Function to better access arrays', () => {
+    it('pulls the correct index using an id', () => {
+        expect(inventoryList.find(item => item.itemName === "chips").itemMarkdown.decrease).to.equal(1)
+
+        expect(inventoryList.find(item => item.itemName === "whole wheat bread").itemMarkdown.limit).to.equal(5)
+    })
+})
